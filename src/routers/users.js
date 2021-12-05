d074f2aa2f589a51d702467f505c49355b29e883
Mahmoud Naoum <mahmoudnaoum13@gmail.com>
2021-12-05 21:21:25 +0200

    modifing users routers

--- src/routers/users.js
+++ src/routers/users.js
@@ -3,33 +3,73 @@ const User = require('../models/users')
 
 const router = new express.Router()
 
-router.post('/users', (req, res)=> {
+router.post('/users',async (req, res)=> {
     const user = new User(req.body)
-    user.save().then( ()=>{
+
+    try {
+        await user.save()
         res.status(201).send(user)
-    }).catch( (e)=>{
+    } catch (e) {
         res.status(400).send(e)
-    })
+    }
 })
 
-router.get('/users', (req, res)=> {
-    User.find({}).then( (users)=> {
+router.get('/users',async (req, res)=> {
+
+    try {
+        const users = await User.find({})
         res.send(users)
-    }).catch( (e)=> {
-        res.status(500).send()
-    })
+    } catch (error) {
+        res.send(500).send()
+    }
+
 })
 
-router.get('/users/:id', (req, res)=> {
+router.get('/users/:id',async (req, res)=> {
+
     const _id = req.params.id
-    User.findById(_id).then( (user)=> {
+    try {
+        const user = await User.findById(_id)
         if( !user ) {
             return res.status(404).send()
+        } 
+        res.status(200).send(user)
+
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+router.patch('/users/:id', async (req, res) => {
+
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every( (update) => allowedUpdates.includes(update) )
+    if( !isValidOperation ) {
+        return res.status(400).send({ error: 'Invaild updates!'})
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate( req.params.id , req.body , {new: true, runValidators: true})
+        if( !user ) {
+            res.status(404).send()
         }
         res.status(200).send(user)
-    }).catch( (e)=> {
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+        if( !user ) {
+            res.status(404).send()
+        }
+        res.send()
+    } catch (error) {
         res.status(500).send()
-    })
+    }
 })
 
 module.exports = router
