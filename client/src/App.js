4f1b980bee95e42620d0f42c009f36c3d201fdd4
abdumamdouh <eabdogfx@gmail.com>
2021-12-06 17:20:32 +0200

    update Routes with react router 5

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 
 // pages
 import {
@@ -12,17 +17,17 @@ import {
   AllProducts,
   SingleProduct,
   Error,
-  Login,
-} from './pages';
+  Login
+} from "./pages";
 
 // components
-import Navbar from './components/Navbar/Navbar';
-import CartSidebar from './components/CartSidebar/CartSidebar';
-import Sidebar from './components/Sidebar/Sidebar';
-import Footer from './components/Footer/Footer';
+import Navbar from "./components/Navbar/Navbar";
+import CartSidebar from "./components/CartSidebar/CartSidebar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Footer from "./components/Footer/Footer";
 
 // scroll to top component
-import ScrollToTop from './utils/ScrollToTop';
+import ScrollToTop from "./utils/ScrollToTop";
 
 const App = () => {
   return (
@@ -32,37 +37,36 @@ const App = () => {
       <CartSidebar />
       <Sidebar />
       <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-        <Route exact path="/shirts">
-          <Shirts />
-        </Route>
-        <Route exact path="/shoes">
-          <Shoes />
-        </Route>
-        <Route exact path="/headphones">
-          <HeadPhones />
-        </Route>
-        <Route exact path="/about">
-          <About />
-        </Route>
-        <Route exact path="/account">
-          <Contact />
-        </Route>
-        <Route exact path="/cart">
-          <Cart />
-        </Route>
-        <Route exact path="/products">
-          <AllProducts />
-        </Route>
-        <Route exact path="/products/:id" children={<SingleProduct />} />
-        <Route path="*">
-          <Error />
-        </Route>
+        <Route exact path="/" component={Home} name="Home Page" />
+        <Route exact path="/login" component={Login} name="Login Page" />
+        <Route exact path="/shirts" component={Shirts} name="Shirts Page" />
+        <Route exact path="/shoes" component={Shoes} name="Shoes Page" />
+        <Route
+          exact
+          path="/headphones"
+          component={HeadPhones}
+          name="HeadPhones Page"
+        />
+        <Route exact path="/about" component={About} name="About Page" />
+        <Route exact path="/account" component={Contact} name="Contact Page" />
+        <Route exact path="/cart" component={Cart} name="Cart Page" />
+        <Route
+          exact
+          path="/products"
+          component={AllProducts}
+          name="AllProducts Page"
+        />
+        <Route
+          exact
+          path="/products/:id"
+          component={SingleProduct}
+          name="SingleProduct Page"
+        />
+
+        {/* <Route exact path="/products/:id" children={<SingleProduct />} /> */}
+
+        <Route exact path="/404" component={Error} name="Error Page" />
+        <Redirect to="/404" />
       </Switch>
       <Footer />
     </Router>
