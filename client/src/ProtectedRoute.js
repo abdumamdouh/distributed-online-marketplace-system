74f30ad28fa4e11f61754e17c91985c5d9daccda
abdumamdouh <eabdogfx@gmail.com>
2021-12-06 17:42:39 +0200

    implement protected route

---
+++ client/src/ProtectedRoute.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Redirect, Route } from "react-router";
+
+import { useSelector } from "react-redux";
+
+const ProtectedRoute = ({ component: Component, exact, path }) => {
+  const { userInfo } = useSelector((state) => state.users);
+
+  return (
+    <Route
+      exact={exact}
+      path={path}
+      render={(props) =>
+        // this.props.authedUser
+        !userInfo ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            // to property pass info to be used in withRouter props
+            // we will pass from state represnts the location of the current route
+            to={{
+              pathname: "/login",
+              state: {
+                from: props.location
+              }
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default ProtectedRoute;
