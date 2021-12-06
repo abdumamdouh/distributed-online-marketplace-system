8e93709231f5d8207dba31d0a7cc33c10db2af6e
abdumamdouh <eabdogfx@gmail.com>
2021-12-06 15:11:20 +0200

    Login & Sign up

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -7,43 +7,92 @@ const LoginPage = () => {
     <>
       <section className="py-5">
         <div className="container">
-          <Title title="Login" />
-          <div className="row">
+          <Title title="Login with existing account" />
+          <div className="row" style={{ marginBottom: "40px" }}>
             <div className="col-10 col-md-9 mx-auto">
               <div className="contact-form">
-                <h4 className="contact-form__title">Manage Inventory</h4>
                 <form
                   onSubmit={(e) => e.preventDefault()}
                   className="contact-form__form"
                 >
-                  <div className="contact-form__input d-flex justify-content-center align-items-center">
+                  <div
+                    className="contact-form__input d-flex justify-content-center align-items-center"
+                    style={{ margin: "5px" }}
+                  >
+                    <input
+                      className="contact-form__input-field"
+                      type="text"
+                      placeholder="email"
+                    />
+                  </div>
+                  <div
+                    className="contact-form__input d-flex justify-content-center align-items-center"
+                    style={{ margin: "5px" }}
+                  >
+                    <input
+                      className="contact-form__input-field"
+                      type="text"
+                      placeholder="Password"
+                    />
+                  </div>
+                  <div className="contact-form__message mt-4"></div>
+                  <button
+                    type="submit"
+                    className="btn"
+                    style={{ "margin-left": "10px", alignItems: "center" }}
+                  >
+                    Login
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+
+          <Title title="Create a new Account" style={{ marginTop: "10px" }} />
+          <div className="row">
+            <div className="col-10 col-md-9 mx-auto">
+              <div className="contact-form">
+                <form
+                  onSubmit={(e) => e.preventDefault()}
+                  className="contact-form__form"
+                >
+                  <div
+                    className="contact-form__input d-flex justify-content-center align-items-center"
+                    style={{ margin: "5px" }}
+                  >
                     <input
                       className="contact-form__input-field"
                       type="text"
                       placeholder="Name"
                     />
+                  </div>
+                  <div
+                    className="contact-form__input d-flex justify-content-center align-items-center"
+                    style={{ margin: "5px" }}
+                  >
                     <input
                       className="contact-form__input-field"
                       type="text"
-                      placeholder="Price"
+                      placeholder="Email"
                     />
                   </div>
-                  <div className="contact-form__message mt-4">
-                    <textarea
-                      rows="10"
-                      className="contact-form__message"
-                      placeholder="description"
-                    ></textarea>
+                  <div
+                    className="contact-form__input d-flex justify-content-center align-items-center"
+                    style={{ margin: "5px" }}
+                  >
+                    <input
+                      className="contact-form__input-field"
+                      type="text"
+                      placeholder="Password"
+                    />
                   </div>
-                  <button type="submit" className="btn">
-                    Add Item
-                  </button>
+                  <div className="contact-form__message mt-4"></div>
                   <button
                     type="submit"
                     className="btn"
                     style={{ "margin-left": "10px" }}
                   >
-                    View Inventory
+                    Sign up
                   </button>
                 </form>
               </div>
