55bbd02c3912f1f4975bbeea4cb8a3e4c19b5b9c
salmaehab <salmaiihab@gmail.com>
2021-12-06 13:50:02 +0200

    Searchbar fixed

--- client/src/components/Searchbar/Searchbar.js
+++ client/src/components/Searchbar/Searchbar.js
@@ -1,24 +1,24 @@
-// import React from "react";
-// import "./Searchbar.css";
-// import {useState} from "react";
-// import { useSelector } from 'react-redux';
+import React from "react";
+import "./Searchbar.css";
+import {useState} from "react";
+import { useSelector } from 'react-redux';
 
-// const Searchbar = ({products}) => {
-//  const [searchText, setSearchText] = useState("");
-//   return (
-//     <div>
-//       <input
-//         type="text"
-//         placeholder="Search..."
-//         className="searchbar"
-//         onChange={e => setSearchText(e.target.value)}
-//       />
-//       {products.filter(item =>{
-//         if(searchText=="")
-//         {return item}
-//       })}
-//     </div>
-//   );
-// };
+const Searchbar = ({products}) => {
+ const [searchText, setSearchText] = useState("");
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        className="searchbar"
+      //  onChange={e => setSearchText(e.target.value)}
+      />
+      {/* {products.filter(item =>{
+        if(searchText=="")
+        {return item}
+      })} */}
+    </div>
+  );
+};
 
-// export default Searchbar;
+export default Searchbar;
