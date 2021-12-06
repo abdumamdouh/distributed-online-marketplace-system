b798c57a2db6f948aabe8a324cdd096eb73137f8
abdumamdouh <eabdogfx@gmail.com>
2021-12-06 14:49:53 +0200

    Login Form first look

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,42 +1,52 @@
-import ContactInfo from "../components/ContactUs/ContactInfo/ContactInfo";
-import ContactForm from "../components/ContactUs/ContactForm/ContactForm";
 import Title from "../components/Title/Title";
-import Stores from "../components/Stores/Stores";
 
-import store1 from "../assets/images/store-1.jpg";
-import store2 from "../assets/images/store-2.jpg";
-import store3 from "../assets/images/store-3.jpg";
-import store4 from "../assets/images/store-4.jpg";
+import "../components/ContactUs/ContactForm/ContactForm.scss";
 
 const LoginPage = () => {
   return (
     <>
       <section className="py-5">
         <div className="container">
-          <Title title="Profile" />
+          <Title title="Login" />
           <div className="row">
-            <div className="col-10 col-md-3 mx-auto">
-              <ContactInfo />
-            </div>
             <div className="col-10 col-md-9 mx-auto">
-              <ContactForm />
-            </div>
-          </div>
-          <div className="pt-5 pb-4">
-            <Title title="OUR STORES" />
-          </div>
-          <div className="row">
-            <div className="col-10 col-md-3 mx-auto">
-              <Stores image={store4} title="new york" />
-            </div>
-            <div className="col-10 col-md-3 mx-auto">
-              <Stores image={store2} title="london" />
-            </div>
-            <div className="col-10 col-md-3 mx-auto">
-              <Stores image={store3} title="oslo" />
-            </div>
-            <div className="col-10 col-md-3 mx-auto">
-              <Stores image={store1} title="Egypt" />
+              <div className="contact-form">
+                <h4 className="contact-form__title">Manage Inventory</h4>
+                <form
+                  onSubmit={(e) => e.preventDefault()}
+                  className="contact-form__form"
+                >
+                  <div className="contact-form__input d-flex justify-content-center align-items-center">
+                    <input
+                      className="contact-form__input-field"
+                      type="text"
+                      placeholder="Name"
+                    />
+                    <input
+                      className="contact-form__input-field"
+                      type="text"
+                      placeholder="Price"
+                    />
+                  </div>
+                  <div className="contact-form__message mt-4">
+                    <textarea
+                      rows="10"
+                      className="contact-form__message"
+                      placeholder="description"
+                    ></textarea>
+                  </div>
+                  <button type="submit" className="btn">
+                    Add Item
+                  </button>
+                  <button
+                    type="submit"
+                    className="btn"
+                    style={{ "margin-left": "10px" }}
+                  >
+                    View Inventory
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
