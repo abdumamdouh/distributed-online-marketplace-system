bd4b4ad74c18376d1efe96faa9ae94a884f94726
abdumamdouh <eabdogfx@gmail.com>
2021-12-08 22:55:30 +0200

    fix login

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -26,7 +26,7 @@ const LoginPage = (props) => {
     };
 
     const path = fromObj.from.pathname;
-    props.history.push(path);
+    props.history.push("/");
   };
 
   const handleSignUpForm = (e) => {
@@ -39,7 +39,7 @@ const LoginPage = (props) => {
     };
 
     const path = fromObj.from.pathname;
-    props.history.push(path);
+    props.history.push("/");
   };
   return (
     <>
